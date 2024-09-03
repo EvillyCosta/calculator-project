27abffa04587f38f5145edefc7b1db980ec5d9da
Evilly Costa <152512061+EvillyCosta@users.noreply.github.com>
2024-09-03 02:33:12 -0300

    Update index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+// importante selecionar oq será utilizado primeiro
+const display = document.getElementById('display');
+
+
+// função para adicionar valor ao display
+function addToDisplay(value) {
+
+    if (value === '=') {
+        display.value = eval(display.value); //eval converte a string e valor
+
+    } else if (value === 'C') {
+        display.value = display.value.slice(0, -1); //.slice, método que extrai uma parte da string, (a(indice), (b)final)
+
+    } else if (value === 'AC') {
+        display.value = '';
+
+    } else {
+        display.value += value;
+    }
+}
+
+// adiciona eventos de clique aos botões
+document.querySelectorAll('.a').forEach(button => { //a forEach percorre todos os botões da calculadora e adiciona um ouvinte de eventos de clique a cada um
+    button.addEventListener('click', function(event) {
+        event.preventDefault(); //previne o comportamento padrão do link, o "a" no caso
+        const value = this.textContent; //pega o valor do botão clicado
+        addToDisplay(value); 
+    });
+});
